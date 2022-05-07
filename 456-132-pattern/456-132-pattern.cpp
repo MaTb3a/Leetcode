32a13623707e79ff8fb6b83e9a857321c1f5db74
hossam hakim <38508098+MaTb3aa@users.noreply.github.com>
2022-05-07 04:58:05 +0200

    Time: 445 ms (5.17%), Space: 71.9 MB (5.82%) - LeetHub

--- 456-132-pattern/456-132-pattern.cpp
+++ 456-132-pattern/456-132-pattern.cpp
@@ -2,19 +2,17 @@ class Solution {
 public:
     bool find132pattern(vector<int>& nums) {
         map<int,int>mpk;
-        set<int>sti,stk;
+        set<int>stk;
         
         for(auto t : nums)mpk[t]++,stk.insert(t);
+        int i = nums[0];
         
         for(int j=0;j<nums.size();j++){
-            auto iti = sti.begin();
-            if(iti != sti.end()){
-                auto itk = stk.upper_bound(*iti);
-                if(itk!=stk.end() && *iti < *itk && *itk  <nums[j] )return 1;  
-            }
-                sti.insert(nums[j]);
-                mpk[nums[j]]--;
-                if(mpk[nums[j]]==0)stk.erase(nums[j]);  
+            auto itk = stk.upper_bound(i);
+            if(itk!=stk.end() && i < *itk && *itk  <nums[j] )return 1;  
+            i = min(i,nums[j]);
+            mpk[nums[j]]--;
+            if(mpk[nums[j]]==0)stk.erase(nums[j]);  
         }
         return 0;
     }    
