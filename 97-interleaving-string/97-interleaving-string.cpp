d9f67b79071594eab5d4c88aa0d74f1dc6fd113e
hossam hakim <38508098+MaTb3aa@users.noreply.github.com>
2022-07-08 04:59:26 +0200

    Time: 5 ms (65.51%), Space: 6.2 MB (89.95%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,20 +1,34 @@
-class Solution {
-public:
-    int dp[101][101];
-    bool solve(int i,int j,int k,string &s1, string &s2, string &s3){
-        if(i+j==s3.size())
-            return 1;
-        int &ret=dp[i][j];
-        if(~ret)return ret;
-        ret=0;
-        if(s1[i]==s3[k])
-            ret|=solve(i+1,j,k+1,s1,s2,s3);
-        if(s2[j]==s3[k])
-            ret|=solve(i,j+1,k+1,s1,s2,s3);
-        return ret;
-    }
-    bool isInterleave(string s1, string s2, string s3) {
-        memset(dp,-1,sizeof dp);
-        return s1.size()+s2.size()== s3.size() ? solve(0,0,0,s1,s2,s3) : 0;
-    }
+
+    class Solution { 
+public: 
+    int dp[102][102]; 
+    int x,y,z; 
+    bool solve (string& s1, string& s2, string &s3, int a, int b, int c) 
+    { 
+        if(a==x && b==y && c==z)  
+            return true; 
+        int &ret=dp[a][b];
+        
+        if(ret !=-1) 
+            return ret; 
+       ret=0; 
+       if(a<x && s1[a]==s3[c]) 
+            ret |= solve(s1, s2, s3, a+1, b, c+1); 
+           
+       if(b<y &&s2[b]==s3[c]) 
+            ret |= solve(s1, s2, s3, a, b+1, c+1); 
+         
+          
+        return ret; 
+    } 
+    bool isInterleave(string s1, string s2, string s3) { 
+        if (s1.size()+s2.size()!= s3.size()) 
+            return false ; 
+        x=s1.size(); 
+        y=s2.size(); 
+        z=s3.size(); 
+        memset(dp, -1, sizeof(dp)); 
+        return solve(s1, s2, s3,0,0,0); 
+         
+    } 
 };
