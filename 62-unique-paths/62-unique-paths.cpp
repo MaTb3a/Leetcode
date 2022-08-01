6ce6ba39fb9ed98ae41b18f6be5c7636e7102c46
hossam hakim <38508098+MaTb3aa@users.noreply.github.com>
2022-08-01 11:27:11 +0200

    Time: 0 ms (100.00%), Space: 6.1 MB (59.42%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     long long dp[101][101];
-    long long solve(int i,int j){
-        if(i==1 && j==1)return 1;
-        if(i<= 0||j<=0)return 0;
-        long long &ret=dp[i][j];
-        if(~ret)return ret;
-        ret=0;
-        ret+=solve(i-1,j);
-        ret+=solve(i,j-1);
-        return ret;
-    }
+    
     int uniquePaths(int m, int n) {
-        memset(dp,-1,sizeof dp);
-        return solve(m,n);
+        for(int i=0;i<m;i++)
+            dp[i][0]=1;
+        for(int i=0;i<n;i++)
+            dp[0][i]=1;
+        
+        for(int i=1;i<m;i++)
+            for(int j=1;j<n;j++)
+                dp[i][j]=dp[i-1][j]+dp[i][j-1];
+        return dp[m-1][n-1];
     }
 };
