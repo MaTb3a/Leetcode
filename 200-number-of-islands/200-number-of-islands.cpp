f23863ddac38de0b7835b92e9690a10dc1dd6955
hossam hakim <38508098+MaTb3aa@users.noreply.github.com>
2022-08-29 18:13:04 +0200

    Time: 85 ms (14.62%), Space: 12.4 MB (55.87%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,43 +1,34 @@
 class Solution {
 public:
-    int dx[4]={0,0,1,-1};
-    int dy[4]={1,-1,0,0};
-    int vis[301][301];
+    // connected component
+    
+    bool vis[301][301];
     int n,m;
-    bool valid(int x,int y){
-        return x>=0 && y>=0 && x<n && y <m && !vis[x][y];
-    }
-    void BFS(int x,int y,vector<vector<char>>& grid){
-        queue<pair<int,int>>q;
-        q.push({x,y});
+    
+    void solve(int x,int y,vector<vector<char>>& grid){
+        if( x<0 || y <0 || x>=n || y>=m || vis[x][y] || grid[x][y]=='0')
+            return ;
         vis[x][y]=1;
-        while(q.size()){
-            pair<int,int>p=q.front();
-            q.pop();
-            for(int i=0;i<4;i++){
-                int ddx=dx[i]+p.first;
-                int ddy=dy[i]+p.second;
-                if(valid(ddx,ddy) && grid[ddx][ddy]=='1'){
-                    vis[ddx][ddy]=1;
-                    q.push({ddx,ddy});
-                }
-            }
-        }
+        
+        solve(x+1,y,grid);
+        solve(x-1,y,grid);
+        solve(x,y+1,grid);
+        solve(x,y-1,grid);
+        
     }
     
     int numIslands(vector<vector<char>>& grid) {
+        n=grid.size(); // rows
+        m=grid[0].size(); // columns
         int cnt=0;
-        n= grid.size();
-        m= grid[0].size();
-        memset(vis,0,sizeof vis);
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]=='1' && !vis[i][j]){
+        
+        for(int i=0;i<n;i++)
+            for(int j=0;j<m;j++)
+                if(vis[i][j]==0 && grid[i][j]=='1'){
                     cnt++;
-                    BFS(i,j,grid);
+                     solve(i,j,grid);
                 }
-            }
-        }
         return cnt;
+       
     }
 };
