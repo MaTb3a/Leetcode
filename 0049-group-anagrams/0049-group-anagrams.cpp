834980564760162b456418aedd98242ab750a001
hossam hakim <38508098+MaTb3aa@users.noreply.github.com>
2024-02-06 17:02:17 +0200

    Time: 30 ms (60.89%), Space: 24.4 MB (25.67%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>>ans;
+        map<string,vector<string>>mp;
+        for(auto t : strs){
+            string tmp = t;
+            sort(tmp.begin(),tmp.end());
+            mp[tmp].push_back(t);
+        }
+        for(auto t : mp)ans.push_back(t.second);
+        return ans;
+    }
+};
