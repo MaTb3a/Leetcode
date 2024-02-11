ad411c40e54d9f6f782a4619b2c3c8779f7c27d3
hossam hakim <38508098+MaTb3aa@users.noreply.github.com>
2024-02-11 20:08:33 +0200

    Time: 60 ms (72.04%), Space: 13.7 MB (75.15%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -9,18 +9,20 @@ public:
         if(~ret)return ret;
         ret = -1e9;
         
-        ret=max(ret,solve(x1+1,y1,x2+1,y2,grid)+ ((x1==x2 && y1 == y2) ? grid[x1][y1] : grid[x1][y1]+grid[x2][y2]));
-        ret=max(ret,solve(x1+1,y1,x2+1,y2+1,grid)+ ((x1==x2 && y1 == y2) ? grid[x1][y1] : grid[x1][y1]+grid[x2][y2]));
-        ret=max(ret,solve(x1+1,y1,x2+1,y2-1,grid)+ ((x1==x2 && y1 == y2) ? grid[x1][y1] : grid[x1][y1]+grid[x2][y2]));
+        ret=max(ret,solve(x1+1,y1,x2+1,y2,grid));
+        ret=max(ret,solve(x1+1,y1,x2+1,y2+1,grid));
+        ret=max(ret,solve(x1+1,y1,x2+1,y2-1,grid));
         
-        ret=max(ret,solve(x1+1,y1+1,x2+1,y2,grid)+ ((x1==x2 && y1 == y2) ? grid[x1][y1] : grid[x1][y1]+grid[x2][y2]));
-        ret=max(ret,solve(x1+1,y1+1,x2+1,y2+1,grid)+ ((x1==x2 && y1 == y2) ? grid[x1][y1] : grid[x1][y1]+grid[x2][y2]));
-        ret=max(ret,solve(x1+1,y1+1,x2+1,y2-1,grid)+ ((x1==x2 && y1 == y2) ? grid[x1][y1] : grid[x1][y1]+grid[x2][y2]));
+        ret=max(ret,solve(x1+1,y1+1,x2+1,y2,grid));
+        ret=max(ret,solve(x1+1,y1+1,x2+1,y2+1,grid));
+        ret=max(ret,solve(x1+1,y1+1,x2+1,y2-1,grid));
 
-        ret=max(ret,solve(x1+1,y1-1,x2+1,y2-1,grid)+ ((x1==x2 && y1 == y2) ? grid[x1][y1] : grid[x1][y1]+grid[x2][y2]));
-        ret=max(ret,solve(x1+1,y1-1,x2+1,y2+1,grid)+ ((x1==x2 && y1 == y2) ? grid[x1][y1] : grid[x1][y1]+grid[x2][y2]));
-        ret=max(ret,solve(x1+1,y1-1,x2+1,y2,grid)+ ((x1==x2 && y1 == y2) ? grid[x1][y1] : grid[x1][y1]+grid[x2][y2]));
-
+        ret=max(ret,solve(x1+1,y1-1,x2+1,y2-1,grid));
+        ret=max(ret,solve(x1+1,y1-1,x2+1,y2+1,grid));
+        ret=max(ret,solve(x1+1,y1-1,x2+1,y2,grid));
+        
+        int add = ((x1==x2 && y1 == y2) ? grid[x1][y1] : grid[x1][y1]+grid[x2][y2]);
+        ret=max(ret,ret+add);
         return ret ;
     }
     int cherryPickup(vector<vector<int>>& grid) {
