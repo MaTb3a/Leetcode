e9eae8f4c0e514e8a5119700f9d0b1560adcdbb2
hossam hakim <38508098+MaTb3aa@users.noreply.github.com>
2024-02-22 18:32:49 +0200

    Time: 135 ms (65.93%), Space: 72.4 MB (21.11%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -2,12 +2,13 @@ class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
         vector<int>v(n+1);
-        for(auto t : trust)v[t[1]]++;
-        int mx = *max_element(v.begin(),v.end());
-        int val = -1;
-        if(mx != n-1)return -1;
-        for(int i = 0 ; i <= n ;i++)if(v[i] == mx)val = i;
-        for(auto t : trust)if(t[0]==val)return -1;
-        return val;
+        for(auto t : trust){
+            v[t[1]]++;
+            v[t[0]]--;
+        }
+        for(int i =1 ;i<=n;i++)
+            if(v[i]==n-1)return i;
+        return -1;
+       
     }
 };
