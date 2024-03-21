69d7711e8386f9e1aa10e40f04fce9fba4b7358c
hossam hakim <38508098+MaTb3aa@users.noreply.github.com>
2024-03-21 15:36:35 +0200

    Time: 96 ms (14.94%), Space: 47.5 MB (18.84%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        int freq[26] = {0};
-        for(char task : tasks){
-            freq[task - 'A']++;
+        vector<int>vis(26);
+        for(auto t : tasks)vis[t-'A']++;
+        priority_queue<int>pr;
+        for(auto t : vis)if(t)pr.push(t);
+        
+        int ans = 0;
+        while(pr.size()){
+            int cycle = n+1;
+            int cnt = 0;
+            vector<int>store;
+            while(cycle-- && pr.size()){
+                if(pr.top()>1)store.push_back(pr.top()-1);
+                pr.pop();
+                cnt++;
+            }
+            for(auto t : store)pr.push(t);
+            ans+=(pr.empty() ? cnt : n+1);
         }
-        sort(begin(freq) , end(freq));
-        int chunk = freq[25] - 1;
-        int idel = chunk * n;
-
-        for(int i=24; i>=0; i--){
-            idel -= min(chunk,freq[i]);
-        }
-
-        return idel < 0 ? tasks.size() : tasks.size() + idel;
+        return ans;
     }
 };
