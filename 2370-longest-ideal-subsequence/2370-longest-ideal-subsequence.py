3247f5ae043f02e9e5582033a047f65fef866b01
hossam hakim <38508098+MaTb3aa@users.noreply.github.com>
2024-04-26 18:32:17 +0300

    Time: 6816 ms (5.11%), Space: 114.1 MB (6.57%) - LeetHub

--- 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.py
+++ 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.py
@@ -1,11 +1,19 @@
 class Solution:
+    def solve(self,idx,lst,k,s,dp):
+        if idx == len(s):
+            return 0
+        ret = -10000000000
+        if dp[idx][lst] != -1:
+            return dp[idx][lst]
+        if abs( (ord(s[idx])-ord('a')) - lst) <= k:
+            ret=max(ret,self.solve(idx+1,(ord(s[idx])-ord('a')),k,s,dp)+1)
+        ret=max(ret,self.solve(idx+1,lst,k,s,dp))
+        dp[idx][lst]=ret
+        return ret
     def longestIdealString(self, s: str, k: int) -> int:
-        dp = [0]*26
-        ans = 1
-
-        for c in s:
-            i = ord(c)-ord('a')
-            dp[i]=1+max(dp[i],max(dp[max(0,i-k):i]+dp[i+1:k+i+1]+[0]))
-            ans = max(ans,dp[i])
-
-        return ans
+        # dp = [[-1] for _ in range(0,26)] * for _ in range(0,len(s))
+        dp = [[-1] * (26) for _ in range(0,len(s))]
+        best = 0
+        for i in range(0,len(s)):
+            best = max(best,self.solve(i+1,(ord(s[i])-ord('a')),k,s,dp)+1)
+        return best
