4370856640fcd0ceb119443c35c4071f85bb0ae2
hossam hakim <38508098+MaTb3aa@users.noreply.github.com>
2024-04-27 18:30:39 +0300

    Time: 205 ms (41.17%), Space: 17.3 MB (48.24%) - LeetHub

--- 0514-freedom-trail/0514-freedom-trail.py
+++ 0514-freedom-trail/0514-freedom-trail.py
@@ -1,16 +1,15 @@
 class Solution:
-    @functools.lru_cache(None)
-    def solve(self,i,j,ring,key):
-        if j == len(key):
-            return 0
-        ret = 1000000
-        for k,ch in enumerate(ring):
-            if ch == key[j]:
-                minDist = min(abs(i-k),len(ring)-abs(i-k))
-                ret = min(ret,self.solve(k,j+1,ring,key)+minDist)
-        return ret
-
-     
     def findRotateSteps(self, ring: str, key: str) -> int:
-        dp = [[-1 for _ in range(0,101)] for _ in range(0,101)]
-        return self.solve(0,0,ring,key)+len(key)
+        n = len(ring)
+        m = len(key)
+        @functools.lru_cache(None)
+        def solve(i,j):
+            if j == m:
+                return 0
+            ret = 1000000
+            for k,ch in enumerate(ring):
+                if ch == key[j]:
+                    minDist = min(abs(i-k),n-abs(i-k))
+                    ret=min(ret,solve(k,j+1)+minDist)
+            return ret
+        return solve(0,0)+m
