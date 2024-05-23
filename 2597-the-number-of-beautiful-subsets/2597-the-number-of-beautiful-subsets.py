afdffae897876d88e28c618ca622c0351379d4a9
hossam hakim <38508098+MaTb3aa@users.noreply.github.com>
2024-05-23 20:18:07 +0300

    Time: 14544 ms (5.07%), Space: 444.8 MB (5.06%) - LeetHub

--- 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.py
+++ 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.py
@@ -5,7 +5,9 @@ class Solution:
                 if abs(i-x) == k :
                     return False
             return True
+        @lru_cache(maxsize=None)
         def solve(i,v):
+            v = list(v)
             if i == len(nums):
                 if len(v) > 0:
                     return 1
@@ -14,10 +16,10 @@ class Solution:
             ret = 0
             if valid(v,nums[i]) :
                 v.append(nums[i])
-                ret+=solve(i+1,v)
+                ret+=solve(i+1,tuple(v))
                 v.pop()
 
-            ret+=solve(i+1,v)
+            ret+=solve(i+1,tuple(v))
             return ret
-        return solve(0,[])
+        return solve(0,())
         
