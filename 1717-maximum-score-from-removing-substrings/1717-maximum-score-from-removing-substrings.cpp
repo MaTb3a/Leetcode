79d2fc2de86e39c7840d76acaead0bf848e8e52c
hossam hakim <38508098+MaTb3aa@users.noreply.github.com>
2024-07-12 18:20:18 +0300

    Time: 87 ms (58.13%), Space: 26.1 MB (38.62%) - LeetHub

---
+++ 1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+        // ab | ba
+    int solve(string &s,string remove,int val){
+        string newS = "";
+        int score = 0;
+        for(int i = 0 ; i < s.size();i++){
+            if(!newS.empty() && newS.back() ==  remove[0] && s[i] == remove[1]){
+                score+=val;
+                newS.pop_back();
+            }
+            else newS.push_back(s[i]);
+        }
+        s = newS;
+        return score;
+    }
+    int maximumGain(string s, int x, int y) {
+         
+       if(x > y)
+            return solve(s,"ab",x) + solve(s,"ba",y);
+
+        return  solve(s,"ba",y) + solve(s,"ab",x);
+
+    }
+};
