f1625bd1ce010f98f4405cab7948a56078e421da
hossam hakim <38508098+MaTb3aa@users.noreply.github.com>
2024-07-14 20:38:12 +0300

    Time: 0 ms (100.00%), Space: 10.1 MB (21.01%) - LeetHub

---
+++ 0726-number-of-atoms/0726-number-of-atoms.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    string countOfAtoms(string formula) {
+         
+            stack<map<string,int>>stk;
+
+            stk.push(map<string,int>());
+            int n = formula.size();
+            int idx = 0 ;
+            while(idx < n){
+                if(formula[idx] == '(')
+                    stk.push(map<string,int>()),idx++;
+                else if(formula[idx] == ')') {
+                    int num = 0;idx++;
+                    while(idx < n && isdigit(formula[idx]))
+                        num = num * 10 + (formula[idx++]-'0');
+                    auto curMap = stk.top();
+                    stk.pop();
+                    if(!num)num=1;
+                    for(auto it : curMap){
+                        stk.top()[it.first] += it.second * num;
+                    }   
+                }
+                else {
+                    string atom = "";
+                    atom+=formula[idx++];
+                    while(idx < n && islower(formula[idx]))
+                        atom+=formula[idx++];
+                    int num = 0;
+                    while(idx < n && isdigit(formula[idx]))
+                        num = num * 10 + (formula[idx++]-'0');
+                    if(!num)num=1;
+                    stk.top()[atom] += num;
+                }
+            }
+            string ans = "";
+            for(auto it : stk.top()){
+                ans+=it.first + (it.second > 1 ? to_string(it.second) : "");
+            }
+            return ans;
+    }
+};
