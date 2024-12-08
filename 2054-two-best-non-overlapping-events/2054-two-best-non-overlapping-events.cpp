dab9c4240a14fd8972f92a916a781f01202a9060
Hossam Hakim <38508098+MaTb3aa@users.noreply.github.com>
2024-12-08 20:14:02 +0200

    Time: 63 ms (71.62%), Space: 126.1 MB (75.41%) - LeetHub

--- 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.cpp
+++ 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     int maxTwoEvents(vector<vector<int>>& events) {
-       
-        vector<vector<int>>v;
-        for(auto time : events){
-            v.push_back({time[0],0,time[2]});
-            v.push_back({time[1],1,time[2]});
-        }
-        sort(v.begin(),v.end());
+        priority_queue<pair<int,int>>pr; // f ,s  => (end , valu)
+        sort(events.begin(),events.end());
         int mx = 0, mxV = 0;
-        for(auto it : v){
-            if(it[1]==1)  mxV=max(mxV,it[2]);
-            else mx=max(mx,it[2]+mxV);
+        for(int i = 0 ; i < events.size();i++){
+            //               old event (end). | cur  (st)
+            while(pr.size() && -pr.top().first < events[i][0]){
+                mxV=max(mxV,pr.top().second);
+                pr.pop();
+            }
+            mx=max(mx,events[i][2]+mxV);
+            pr.push({-events[i][1],events[i][2]});
         }
         return mx;
     }
-    
 };
