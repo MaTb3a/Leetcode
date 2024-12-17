970fbd11fa59702ee106f195a22df1ff58ad237d
Hossam Hakim <38508098+MaTb3aa@users.noreply.github.com>
2024-12-17 21:42:50 +0200

    Time: 244 ms (48.23%), Space: 19.3 MB (30.31%) - LeetHub

--- 2182-construct-string-with-repeat-limit/2182-construct-string-with-repeat-limit.py
+++ 2182-construct-string-with-repeat-limit/2182-construct-string-with-repeat-limit.py
@@ -1,31 +1,24 @@
-import java.util.*;
-
-class Solution {
-    public String repeatLimitedString(String s, int repeatLimit) {
-        int[] freq = new int[26];
-        for (char c : s.toCharArray()) freq[c - 'a']++;
-
-        PriorityQueue<Character> pq = new PriorityQueue<>(Collections.reverseOrder());
-        for (int i = 0; i < 26; i++) if (freq[i] > 0) pq.add((char) (i + 'a'));
-
-        StringBuilder result = new StringBuilder();
-
-        while (!pq.isEmpty()) {
-            char largest = pq.poll();
-            int idx = largest - 'a';
-            int count = Math.min(repeatLimit, freq[idx]);
-            result.append(String.valueOf(largest).repeat(count));
-            freq[idx] -= count;
-
-            if (freq[idx] > 0 && !pq.isEmpty()) {
-                char nextLargest = pq.poll();
-                result.append(nextLargest);
-                freq[nextLargest - 'a']--;
-                if (freq[nextLargest - 'a'] > 0) pq.add(nextLargest);
-                pq.add(largest);
-            }
-        }
-
-        return result.toString();
-    }
-}
+class Solution:
+    def repeatLimitedString(self, s: str, repeatLimit: int) -> str:
+        cnt = Counter(s)
+        n, hp = 0, []
+        for c in sorted(cnt, reverse=True):
+            heappush(hp, (n, c))
+            n += 1
+        ans = ""
+        while hp:
+            if cnt[hp[0][1]] <= repeatLimit:   
+                n1, c1 = heappop(hp)
+                ans += c1 * cnt[c1]
+            else:                              
+                n1, c1 = heappop(hp)
+                ans += c1 * repeatLimit
+                cnt[c1] -= repeatLimit
+                if hp:                         
+                    n2, c2 = heappop(hp)
+                    ans += c2
+                    cnt[c2] -= 1
+                    if cnt[c2]:
+                        heappush(hp, (n2, c2)) 
+                    heappush(hp, (n1, c1))     
+        return ans
