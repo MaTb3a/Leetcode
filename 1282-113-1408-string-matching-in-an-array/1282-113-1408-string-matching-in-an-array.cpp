54de3ae7b65c7fb4e246c699f74e367fad3f390e
Hossam Hakim <38508098+MaTb3aa@users.noreply.github.com>
2025-01-07 18:32:34 +0200

    Time: 3 ms, Memory: 13.3 MB - LeetHub

---
+++ 1282-113-1408-string-matching-in-an-array/1282-113-1408-string-matching-in-an-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<string> stringMatching(vector<string>& words) {
+        vector<string>v;
+        sort(words.begin(),words.end(),[&](string a,string b){
+            return a.size() < b.size();
+        });
+        for(int i = 0 ; i < words.size();i++)
+            for(int j = i+1 ; j < words.size();j++)
+                if(i != j && words[j].find(words[i]) != string :: npos){
+                    v.push_back(words[i]);
+                    break;
+                }
+        return v; // Trie = KMP
+    }
+};
