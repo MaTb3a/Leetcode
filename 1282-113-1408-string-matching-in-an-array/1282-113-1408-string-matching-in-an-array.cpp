f8aa5a4dddf199f23d641df4deeb480d2ce084e8
Hossam Hakim <38508098+MaTb3aa@users.noreply.github.com>
2025-01-07 18:34:30 +0200

    Time: 6 ms, Memory: 29.1 MB - LeetHub

--- 1282-113-1408-string-matching-in-an-array/1282-113-1408-string-matching-in-an-array.cpp
+++ 1282-113-1408-string-matching-in-an-array/1282-113-1408-string-matching-in-an-array.cpp
@@ -1,16 +1,38 @@
 class Solution {
 public:
+    struct Trie{
+        Trie* vis[26];
+        int frq = 0;
+    }*trie;
+
+    void insert(string s){
+        Trie * top = trie;
+        for(char ch : s){
+            if(!top->vis[ch-'a'])
+                top->vis[ch-'a'] = new Trie();
+            top = top->vis[ch-'a'];
+            top->frq++;
+        }
+    }
+    bool get(string s){
+        Trie* top = trie;
+        for(char ch : s)
+            top = top->vis[ch-'a'];
+        
+        return top->frq > 1;
+    }
+
     vector<string> stringMatching(vector<string>& words) {
-        vector<string>v;
-        sort(words.begin(),words.end(),[&](string a,string b){
-            return a.size() < b.size();
-        });
+        trie = new Trie();
         for(int i = 0 ; i < words.size();i++)
-            for(int j = i+1 ; j < words.size();j++)
-                if(i != j && words[j].find(words[i]) != string :: npos){
-                    v.push_back(words[i]);
-                    break;
-                }
-        return v; // Trie = KMP
+            for(int j = 0 ; j < words[i].size();j++)
+                insert(words[i].substr(j));
+        
+            
+        vector<string>v;
+        for(int i = 0 ; i < words.size();i++)
+            if(get(words[i]))v.push_back(words[i]);
+        
+        return v;
     }
 };
