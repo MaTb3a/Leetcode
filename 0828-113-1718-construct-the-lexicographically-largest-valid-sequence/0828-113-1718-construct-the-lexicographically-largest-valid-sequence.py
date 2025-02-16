af84b3919c732cf330155d68a1978862b592f084
Hossam Hakim <38508098+MaTb3aa@users.noreply.github.com>
2025-02-16 12:56:57 +0200

    Time: 5 ms, Memory: 17.9 MB - LeetHub

--- 0828-113-1718-construct-the-lexicographically-largest-valid-sequence/0828-113-1718-construct-the-lexicographically-largest-valid-sequence.py
+++ 0828-113-1718-construct-the-lexicographically-largest-valid-sequence/0828-113-1718-construct-the-lexicographically-largest-valid-sequence.py
@@ -1,27 +1,27 @@
 class Solution:
     def constructDistancedSequence(self, n: int) -> List[int]:
-        
-        def solve(idx,n,vis,v):
+        v = [0]*(2*n-1)
+        vis = set()
+        def solve(idx,n):
             if idx == len(v):
                 return True
             if v[idx] != 0:
-                return solve(idx+1,n,vis,v)
+                return solve(idx+1,n)
             for i in range(n,0,-1):
-                if vis[i] == True:
+                if i in vis:
                     continue
                 nxt = idx
                 if i >1:
                     nxt+=i
                 if nxt >= 2*n-1 or v[nxt] != 0:
                     continue
-                vis[i] = True
+                vis.add(i)
                 v[idx] = v[nxt] = i
-                if solve(idx+1,n,vis,v) == True:
+                if solve(idx+1,n) == True:
                     return True
-                vis[i] = False
+                vis.remove(i)
                 v[idx] = v[nxt] = 0
             return False
-        v = [0]*(2*n-1)
-        vis = [False] *(n+1)
-        solve(0,n,vis,v)
+       
+        solve(0,n)
         return v
