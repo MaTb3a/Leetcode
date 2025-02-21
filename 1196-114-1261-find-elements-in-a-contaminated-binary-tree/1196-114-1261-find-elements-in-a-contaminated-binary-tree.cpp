0173b541ae7a34e442009fcf95c9832bceecc9a1
Hossam Hakim <38508098+MaTb3aa@users.noreply.github.com>
2025-02-21 12:02:03 +0200

    Time: 1 ms, Memory: 25.5 MB - LeetHub

---
+++ 1196-114-1261-find-elements-in-a-contaminated-binary-tree/1196-114-1261-find-elements-in-a-contaminated-binary-tree.cpp
@@ -0,0 +1,20 @@
+
+class FindElements {
+public:
+    TreeNode* tree;
+    FindElements(TreeNode* root) {
+        tree = root;
+    }
+    
+    bool find(int target) {
+        bitset<21>bt(target+1);
+        int idx = 20;
+        while(!bt[idx])idx--;
+        TreeNode* cp = tree;
+        while(--idx >=0 && cp)
+            if(bt[idx])cp = cp->right;
+            else cp = cp->left;
+        return idx == -1 && cp;
+        
+    }
+};
