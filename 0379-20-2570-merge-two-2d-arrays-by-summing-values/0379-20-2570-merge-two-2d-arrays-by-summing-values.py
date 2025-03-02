9ad81e908530abbaa564b26db6624f151a6cf536
Hossam Hakim <38508098+MaTb3aa@users.noreply.github.com>
2025-03-02 04:12:27 +0200

    Time: 0 ms, Memory: 17.9 MB - LeetHub

--- 0379-20-2570-merge-two-2d-arrays-by-summing-values/0379-20-2570-merge-two-2d-arrays-by-summing-values.py
+++ 0379-20-2570-merge-two-2d-arrays-by-summing-values/0379-20-2570-merge-two-2d-arrays-by-summing-values.py
@@ -3,14 +3,8 @@ class Solution:
         i,j,n,m = 0,0,len(nums1),len(nums2)
         v = []
         while i < n or j < m :
-            if i == n :
-                id1 = 1001
-            else:
-                id1 = nums1[i][0]
-            if j == m:
-                id2 = 1001
-            else:
-                id2 = nums2[j][0]
+            id1 = nums1[i][0] if i < n else float('inf')
+            id2 = nums2[j][0] if j < m else float('inf')
             if id1 < id2 :
                 v.append(nums1[i])
                 i+=1
@@ -18,8 +12,7 @@ class Solution:
                 v.append(nums2[j])
                 j+=1
             else:
-                nums1[i][1]+=nums2[j][1]
-                v.append(nums1[i])
+                v.append((id1,nums1[i][1]+nums2[j][1]))
                 i+=1
                 j+=1
         return v
