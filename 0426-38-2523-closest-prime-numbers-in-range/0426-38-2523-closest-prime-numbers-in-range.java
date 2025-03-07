b64a3e7659c9ed9aa55750794deb0c6b46586aa3
Hossam Hakim <38508098+MaTb3aa@users.noreply.github.com>
2025-03-07 04:36:40 +0200

    Time: 72 ms, Memory: 44.8 MB - LeetHub

--- 0426-38-2523-closest-prime-numbers-in-range/0426-38-2523-closest-prime-numbers-in-range.java
+++ 0426-38-2523-closest-prime-numbers-in-range/0426-38-2523-closest-prime-numbers-in-range.java
@@ -1,21 +1,34 @@
-class Solution:
-    def closestPrimes(self, left: int, right: int) -> list[int]:
-        vis = [False] * (right + 1)
-        vis[0] = vis[1] = True
-        for i in range(2, int(right**0.5) + 1):
-            if not vis[i]:
-                for j in range(i + i, right + 1, i):
-                    vis[j] = True
-        
-        a, b = -1, -1
-        lst = -1
-        min_gap = float('inf')
-        for i in range(left, right + 1):
-            if vis[i]:
-                continue
-            if lst != -1 and i - lst < min_gap:
-                min_gap = i - lst
-                a, b = lst, i
-            lst = i
-        
-        return [a, b]
+import java.util.Arrays;
+
+class Solution {
+    boolean[] vis;
+
+    public int[] closestPrimes(int left, int right) {
+        sieve(right);
+        int a = -1, b = -1;
+        int lst = -1;
+        int minGap = Integer.MAX_VALUE;
+        for (int i = left; i <= right; i++) {
+            if (vis[i]) continue;
+            if (lst != -1 && i - lst < minGap) {
+                minGap = i - lst;
+                a = lst;
+                b = i;
+            }
+            lst = i;
+        }
+        return new int[]{a, b};
+    }
+
+    private void sieve(int n) {
+        vis = new boolean[n + 1];
+        vis[0] = vis[1] = true;
+        for (int i = 2; i * i <= n; i++) {
+            if (!vis[i]) {
+                for (int j = i + i; j <= n; j += i) {
+                    vis[j] = true;
+                }
+            }
+        }
+    }
+}
