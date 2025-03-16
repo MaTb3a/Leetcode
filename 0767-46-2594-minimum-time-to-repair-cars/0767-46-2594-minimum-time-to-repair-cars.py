50449da916c40ad74d35f0bc026b34baa30740dc
Hossam Hakim <38508098+MaTb3aa@users.noreply.github.com>
2025-03-16 07:51:42 +0200

    Time: 1235 ms, Memory: 21.8 MB - LeetHub

---
+++ 0767-46-2594-minimum-time-to-repair-cars/0767-46-2594-minimum-time-to-repair-cars.py
@@ -0,0 +1,15 @@
+class Solution:
+    def repairCars(self, ranks: List[int], cars: int) -> int:
+        def ok(md,cars):
+            for i in ranks:
+                cars-= int(math.sqrt(md//i))
+            return cars <= 0
+        st,ed,cur,md = 1,1e14,-1,0
+        while st<=ed:
+            md = (st+ed)//2
+            if ok(md,cars):
+                cur = md
+                ed = md -1
+            else:
+                st = md +1
+        return int(cur)
