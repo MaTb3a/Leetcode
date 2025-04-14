b98ba924ed8abb719091a8ea8e836814fe11c3a6
Hossam Hakim <38508098+MaTb3aa@users.noreply.github.com>
2025-04-14 13:33:56 +0200

    Time: 10 ms (36.18%) | Memory: 12.2 MB (5.91%) - LeetSync

--- 1656-count-good-triplets/count-good-triplets.cpp
+++ 1656-count-good-triplets/count-good-triplets.cpp
@@ -1,17 +1,22 @@
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+typedef tree<int,null_type,less_equal<int>,rb_tree_tag,tree_order_statistics_node_update>ordered_set;
+
 class Solution {
 public:
     int countGoodTriplets(vector<int>& arr, int a, int b, int c) {
+        ordered_set st;
+        st.insert(arr[0]);
         int cnt = 0;
-        vector<int>frq(1001);
-        for(int j  = 0 ; j < arr.size() ; j++){
-            for(int k = j +1 ; k < arr.size(); k++){
+        for(int j = 1 ; j < arr.size();j++){
+            for(int k = j+1; k < arr.size();k++){
                 if(abs(arr[j] - arr[k]) > b)continue;
-                int l = max(arr[j]-a,arr[k]-c); // mn i
-                int r = min(arr[j]+a,arr[k]+c); // mx i
-                if(l <=r)
-                    cnt+= frq[min(1000,r)] - ( l > 0 ? frq[l-1] : 0);
+                int mx = min(a+arr[j],c+arr[k]);
+                int mn = max(arr[j]-a,arr[k]-c);
+                if(mn <= mx)cnt+=st.order_of_key(mx+1)-st.order_of_key(mn);
             }
-            for(int i = arr[j];i <= 1000;i++)frq[i]++;
+            st.insert(arr[j]);
         }
         return cnt;
     }
