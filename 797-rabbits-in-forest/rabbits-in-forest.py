575a6957de134848f875e5c6c7e7eac39dacc20b
Hossam Hakim <38508098+MaTb3aa@users.noreply.github.com>
2025-04-20 22:36:30 +0200

    Time: 0 ms (100.00%) | Memory: 18.2 MB (9.12%) - LeetSync

--- 797-rabbits-in-forest/rabbits-in-forest.py
+++ 797-rabbits-in-forest/rabbits-in-forest.py
@@ -1,7 +1,7 @@
 class Solution(object):
     def numRabbits(self, answers):
-        mpp = Counter(answers)
-        total = 0
-        for x in mpp:
-            total += ceil(float(mpp[x]) / (x + 1)) * (x + 1)
-        return int(total)
+        mp = Counter(answers)
+        ans = 0
+        for x in mp:
+            ans += ceil(float(mp[x]) / (x + 1)) * (x + 1)
+        return int(ans)
