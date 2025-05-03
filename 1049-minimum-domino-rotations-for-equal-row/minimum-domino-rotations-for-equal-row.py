a9af7367ec5e06b1b8153801d283bd707a406934
Hossam Hakim <38508098+MaTb3aa@users.noreply.github.com>
2025-05-03 22:51:47 +0300

    Time: 29 ms (78.53%) | Memory: 18.4 MB (67.37%) - LeetSync

--- 1049-minimum-domino-rotations-for-equal-row/minimum-domino-rotations-for-equal-row.py
+++ 1049-minimum-domino-rotations-for-equal-row/minimum-domino-rotations-for-equal-row.py
@@ -1,15 +1,16 @@
 class Solution:
     def minDominoRotations(self, tops: List[int], bottoms: List[int]) -> int:
         def solve(t,b,cur):
-            cnt1,cnt2 = 0,0
+            cnt1 = cnt2 = 0
             for i in range(len(tops)):
                 if cur != t[i] and cur != b[i]:
                     return int(1e9)
-                if cur != b[i]:
-                    cnt1+=1
                 if cur != t[i]:
                     cnt2+=1
+                if cur != b[i]:
+                    cnt1+=1
+                    continue
             return min(cnt1,cnt2)
         
-        ans = min(solve(tops,bottoms,tops[0]),solve(tops,bottoms,bottoms[0]))
+        ans = min(solve(tops,bottoms,bottoms[0]),solve(tops,bottoms,tops[0]))
         return ans if ans != int(1e9) else -1
