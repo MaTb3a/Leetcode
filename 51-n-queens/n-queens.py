aba8e73a9aaaf86f6f038c1598cb56ce91c48cdb
Hossam Abdelhakim <38508098+MaTb3a@users.noreply.github.com>
2025-05-29 17:21:24 +0300

    Time: 3 ms (99.78%) | Memory: 18.3 MB (56.13%) - LeetSync

---
+++ 51-n-queens/n-queens.py
@@ -0,0 +1,27 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        grid = [['.' for _ in range(0,n)] for _ in range(0,n)]
+        ans = []
+        col = [0]*n
+        dig1 = [0]*2*n
+        dig2 = [0]*2*n
+
+        def solve(i,grid):
+            if i == n:
+                a = []
+                for x in grid:
+                    a.append(''.join(x))
+                ans.append(a)
+                return
+            for j in range(0,n):
+                if col[j] == 0 and dig1[i-j+n] == 0 and dig2[i+j] == 0:
+                    col[j] = dig1[i-j+n] = dig2[i+j] = 1
+                    grid[i][j] = 'Q'
+                    solve(i+1,grid)
+                    grid[i][j] = '.'
+                    col[j] = dig1[i-j+n] = dig2[i+j] = 0
+        solve(0,grid)
+        return ans
+
+        
+
