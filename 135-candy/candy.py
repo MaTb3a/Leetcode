de8c134b6dfa1f9f9a83d6b35ba4562e37947eef
Hossam Abdelhakim <38508098+MaTb3a@users.noreply.github.com>
2025-06-02 20:56:29 +0300

    Time: 19 ms (41.24%) | Memory: 19.9 MB (36.16%) - LeetSync

--- 135-candy/candy.py
+++ 135-candy/candy.py
@@ -7,6 +7,6 @@ class Solution:
                 ans[i]= ans[i-1]+1
         
         for i in range(n-2,-1,-1):
-            if ratings[i] > ratings[i+1] and ans[i] <= ans[i+1]:
-                ans[i] = ans[i+1]+1 
+            if ratings[i] > ratings[i+1]:
+                ans[i] = max(ans[i],ans[i+1]+1)
         return sum(ans)
