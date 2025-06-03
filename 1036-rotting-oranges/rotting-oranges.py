725e9dcd9fb018501a56d635d9bd0f773235fb7f
Hossam Abdelhakim <38508098+MaTb3a@users.noreply.github.com>
2025-06-03 14:20:12 +0300

    Time: 1 ms (90.03%) | Memory: 18 MB (6.10%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,7 +1,7 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         n,m = len(grid),len(grid[0])
-        q = []
+        q = deque()
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == 2:
@@ -9,7 +9,7 @@ class Solution:
         time = 0
         di = [-1,0,1,0,-1]
         while q:
-            x,y,d = q.pop(0)
+            x,y,d = q.popleft()
             time = max(time,d)
             for i in range(4):
                 dx = di[i]+x
