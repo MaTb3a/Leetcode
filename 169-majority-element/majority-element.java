fc258bcadafb5066b3a7705ee8279c31bfd560b4
Hossam Abdelhakim <38508098+MaTb3a@users.noreply.github.com>
2025-06-14 22:12:16 +0300

    Time: 1 ms (99.85%) | Memory: 53.3 MB (38.97%) - LeetSync

---
+++ 169-majority-element/majority-element.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int ans = 0, cnt = 0;
+        for (int x : nums) {
+            if (cnt == 0) {
+                ans = x;
+                cnt = 1;
+            } else if (ans == x) {
+                cnt++;
+            } else {
+                cnt--;
+            }
+        }
+        return ans;
+    }
+}
