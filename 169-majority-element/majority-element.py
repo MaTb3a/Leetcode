68f29390b4c1ce1ba6127f198039bb13280ce30f
Hossam Abdelhakim <38508098+MaTb3a@users.noreply.github.com>
2025-06-14 22:13:40 +0300

    Time: 7 ms (57.80%) | Memory: 19.4 MB (19.53%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,14 +1,11 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        dic = {}
         ans,cnt = 0,0
         for x in nums:
-            if x not in dic:
-                dic[x] = 1
+            if cnt == 0:
+                ans,cnt = x,1
+            elif ans == x:
+                cnt+=1
             else:
-                dic[x]+=1
-            if dic[x] > cnt:
-                cnt = dic[x]
-                ans = x
-
+                cnt-=1
         return ans
