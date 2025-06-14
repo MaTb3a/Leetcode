24e44d31ee9a3019262497a3180775d0a94a4621
Hossam Abdelhakim <38508098+MaTb3a@users.noreply.github.com>
2025-06-14 19:35:07 +0300

    Time: 11 ms (29.47%) | Memory: 19.5 MB (5.17%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,12 +1,11 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         dic = defaultdict(lambda:0)
+        ans,cnt = 0,0
         for x in nums:
             dic[x]+=1
-        
-        ans,cnt = 0,0
-        for key,value in dic.items():
-            if value > cnt:
-                cnt = value
-                ans = key
+            if dic[x] > cnt:
+                cnt = dic[x]
+                ans = x
+                
         return ans
