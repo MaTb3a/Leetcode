969e66ecaae1abaf0e3b85ad047b423831c2ad0a
Hossam Abdelhakim <38508098+MaTb3a@users.noreply.github.com>
2025-06-28 19:30:15 +0300

    Time: 0 ms (100.00%) | Memory: 18.7 MB (69.91%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -1,30 +1,6 @@
 class Solution:
    
     def maxProfit(self, prices: List[int]) -> int:
-        @cache
-        def solve(i,can):
-            if i == len(prices):
-                return 0
-            if can:
-                ret = max(-prices[i]+solve(i+1,False) , solve(i+1,True))
-            else:
-                ret = max(prices[i]+solve(i+1,True) , solve(i+1,False))
-            return ret
-
-        # 1,2,3,4,5
-        # 7,1,5,3,6,4
-        #   4   3 0 0
-
-      
         
-        # 1 10 100
 
-        # 99
-        # 9 + 90 
-
-        # 7,1,5,3,6,4
-        #     4   3
-        # 1,2,3,4,5. I cannot use approach of first one can you have to sell
-                  
-
-        return solve(0,True)
+        return sum(prices[i] - prices[i-1] if prices[i] > prices[i-1] else 0 for i in range(1,len(prices)))
