2189cb7546b41c48e1a2cd892bb215e357379605
Hossam Oraby <38508098+MaTb3a@users.noreply.github.com>
2025-07-16 14:54:56 +0300

    Time: 3 ms (73.24%) | Memory: 96.2 MB (62.35%) - LeetSync

--- 3490-find-the-maximum-length-of-valid-subsequence-i/find-the-maximum-length-of-valid-subsequence-i.cpp
+++ 3490-find-the-maximum-length-of-valid-subsequence-i/find-the-maximum-length-of-valid-subsequence-i.cpp
@@ -3,14 +3,8 @@ public:
     int maximumLength(vector<int>& nums) {
         int dp[4] ={0};
         for(int x : nums){
-            if(x&1){
-                dp[0] = max(dp[0],dp[1]+1);
-                dp[3]++;
-            }
-            else {
-                dp[1] = max(dp[1],dp[0]+1);
-                dp[2]++;
-            }
+            if(x&1)dp[1] = max(dp[1],dp[0]+1),dp[3]++;
+            else dp[0] = max(dp[0],dp[1]+1),dp[2]++;
         }
         return *max_element(dp,dp+4);
     
