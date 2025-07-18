4238437c51ec88dcc05b4b45cf3f171a677a6556
Hossam Oraby <38508098+MaTb3a@users.noreply.github.com>
2025-07-18 17:37:43 +0300

    Time: 261 ms (33.51%) | Memory: 154.8 MB (54.12%) - LeetSync

--- 2267-minimum-difference-in-sums-after-removal-of-elements/minimum-difference-in-sums-after-removal-of-elements.cpp
+++ 2267-minimum-difference-in-sums-after-removal-of-elements/minimum-difference-in-sums-after-removal-of-elements.cpp
@@ -1,43 +1,33 @@
 class Solution {
 public:
-    vector<long long>calcL(vector<int>& nums){
-        int sz = nums.size(), n = sz/3;
-        vector<long long>v(sz);
-        priority_queue<int>pr;
-        long long sum = 0;
-        for(int i = 0 ; i < sz;i++){
-            sum+=nums[i];
-            pr.push(nums[i]);
-            if(pr.size() > n){
-                sum-=pr.top();pr.pop();
-            }
-            v[i] = sum;
-        }
-        return v;
-    }
-    vector<long long>calcR(vector<int>& nums){
-        int sz = nums.size(), n = sz/3;
-        vector<long long>v(sz);
-        priority_queue<int>pr;
-        long long sum = 0;
-        for(int i = sz-1 ; i >= 0;i--){
-            sum+=nums[i];
-            pr.push(-nums[i]);
-            if(pr.size() > n){
-                sum-=-pr.top();pr.pop();
-            }
-            v[i] = sum;
-        }
-        return v;
-    }
+  
+    
     long long minimumDifference(vector<int>& nums) {
-       vector<long long>l = calcL(nums),r = calcR(nums);
-       int sz = nums.size(),n = sz/3;
-       long long ans = 1e18;
-      
-       for(int i = n ; i <= sz - n;i++){
-        ans=min(ans,l[i-1]-r[i]);
-       }
-       return ans;
+       
+        int sz = nums.size(), n = sz / 3;
+        vector<long long> l(sz),r(sz);
+        long long ans = 1e18;
+        priority_queue<int> prL, prR;
+        long long suml = 0, sumr = 0;
+        for (int i = 0,j = sz - 1 ; i < sz; i++,j--) {
+            suml += nums[i];
+            sumr += nums[j];
+            prL.push(nums[i]);
+            prR.push(-nums[j]);
+
+            if (prL.size() > n) {
+                suml -= prL.top();
+                prL.pop();
+                sumr -= -prR.top();
+                prR.pop();
+            }
+            l[i] = suml;
+            r[j] = sumr;
+        }
+     
+        for (int i = n; i <= sz - n; i++) {
+            ans = min(ans, l[i - 1] - r[i]);
+        }
+        return ans;
     }
 };
