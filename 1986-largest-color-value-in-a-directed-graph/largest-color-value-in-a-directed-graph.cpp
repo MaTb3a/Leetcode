89e96aed1259310faad21b29f7515b4e0b3b284b
Hossam Oraby <38508098+MaTb3a@users.noreply.github.com>
2025-07-23 22:48:28 +0300

    Time: 398 ms (25.98%) | Memory: 188 MB (48.33%) - LeetSync

--- 1986-largest-color-value-in-a-directed-graph/largest-color-value-in-a-directed-graph.cpp
+++ 1986-largest-color-value-in-a-directed-graph/largest-color-value-in-a-directed-graph.cpp
@@ -1,42 +1,38 @@
 class Solution {
 public:
-    int dfs(int node, string& colors, vector<vector<int>>& adj, vector<vector<int>>& count,vector<bool>& visit, vector<bool>& inStack) {
-        if (inStack[node]) {
-            return INT_MAX;
-        }
-        if (visit[node]) {
-            return count[node][colors[node] - 'a'];
-        }
-        visit[node] = true;
-        inStack[node] = true;
-
-        for (auto& neighbor : adj[node]) {
-            if (dfs(neighbor, colors, adj, count, visit, inStack) == INT_MAX) {
-                return INT_MAX;
-            }
-            for (int i = 0; i < 26; i++) {
-                count[node][i] = max(count[node][i], count[neighbor][i]);
-            }
-        }
-        count[node][colors[node] - 'a']++;
-        inStack[node] = false;
-        return count[node][colors[node] - 'a'];
-    }
-
+    
     int largestPathValue(string colors, vector<vector<int>>& edges) {
-        int n = colors.length();
+        queue<int> q;
+        int n = colors.size();
         vector<vector<int>> adj(n);
-        for (auto& edge : edges) {
-            adj[edge[0]].push_back(edge[1]);
+        vector<int> in(n);
+        for (auto it : edges) {
+            adj[it[0]].push_back(it[1]);
+            in[it[1]]++;
         }
 
-        vector<vector<int>> count(n, vector<int>(26));
-        vector<bool> visit(n), inStack(n);
-        int answer = 0;
-        for (int i = 0; i < n; i++) {
-            answer = max(answer, dfs(i, colors, adj, count, visit, inStack));
-        }
+        for (int i = 0; i < n; i++)
+            if (!in[i])
+                q.push(i);
 
-        return answer == INT_MAX ? -1 : answer;
+        int cnt = 0,ans =0;
+        vector<vector<int>>dp(n,vector<int>(26));
+        while (q.size()) {
+            int u = q.front();
+            q.pop();
+            int c = colors[u] - 'a';
+            dp[u][c]++;
+            ans=max(ans,dp[u][c]);
+            cnt++;
+            for (int v : adj[u]) {
+                for(int j = 0 ; j < 26;j++)
+                    dp[v][j] = max(dp[v][j],dp[u][j]);
+                in[v]--;
+                if (!in[v])
+                    q.push(v);
+            }
+        }
+       
+        return cnt == n ? ans:-1;
     }
 };
