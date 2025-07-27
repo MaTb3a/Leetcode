64a9dd14814561e75106089532c831d119512ad9
Hossam Oraby <38508098+MaTb3a@users.noreply.github.com>
2025-07-27 23:14:54 +0300

    Time: 0 ms (100.00%) | Memory: 12.2 MB (50.62%) - LeetSync

--- 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.cpp
+++ 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.cpp
@@ -3,15 +3,17 @@ public:
 
     int countHillValley(vector<int>& nums) {
         int cnt = 0,n = nums.size();
-    
+        // l = 0 , r = 2
+        //2,4,1,1,6,5
+        int l = nums[0];
+       
+
         for(int i = 1 ; i < n-1;i++){
-            if(nums[i] == nums[i-1])continue;
-            int l = i -1;
-            int r = i+1;
-            while (l >= 0 && nums[l] == nums[i])l--;
-            while (r < n && nums[r] == nums[i])r++;
-            if( l >=0 && r < n && nums[i] > nums[l] && nums[i] > nums[r])cnt++;
-            if( l >=0 && r < n && nums[i] < nums[l] && nums[i] < nums[r])cnt++;
+            if(nums[i] != nums[i+1]){
+                if( nums[i] > l && nums[i] > nums[i+1])cnt++;
+                if( nums[i] < l && nums[i] < nums[i+1])cnt++;
+                l = nums[i];
+            }
          }
          return cnt;
 
