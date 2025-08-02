6203d8d3ebde2af4f4d2244faae8a02a88fc9131
Hossam Oraby <38508098+MaTb3a@users.noreply.github.com>
2025-08-02 13:15:14 +0300

    Time: 25 ms (51.36%) | Memory: 23.2 MB (79.67%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -3,13 +3,11 @@ class Solution:
         n = len(nums)
         ans = [1] * n
         
-        # Step 1: Left pass
         left = 1
         for i in range(n):
             ans[i] = left
             left *= nums[i]
 
-        # Step 2: Right pass
         right = 1
         for i in range(n - 1, -1, -1):
             ans[i] *= right
