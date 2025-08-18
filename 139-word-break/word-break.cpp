32f66bdf134ff81a8ee722dae2b30c1d26f326c0
Hossam Oraby <38508098+MaTb3a@users.noreply.github.com>
2025-08-18 18:33:34 +0300

    Time: 7 ms (60.34%) | Memory: 16.1 MB (48.33%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,50 +1,19 @@
-struct TrieNode {
-    bool tail = false;
-    TrieNode *child[26];
-}*trie;
+
 class Solution {
 public:
-    void insert(string s){
-        TrieNode *top = trie;
-        for(char ch : s){
-            if(!top->child[ch-'a'])
-                top->child[ch-'a'] = new TrieNode();
-            top = top->child[ch-'a'];
-        }
-        top->tail = true;
-    }
-
-    bool search(string s){
-        TrieNode *top = trie;
-        for(char ch : s){
-            if(!top->child[ch-'a'])
-                return false;
-            top = top->child[ch-'a'];
-        }
-        return top->tail;
-    }
-    int dp[303][303];
-    int solve(int i,int len,int n,string &s){
-        if(i >= n)return 1;
-        if(i + len > n)return false;
-        
-        int &ret = dp[i][len];
-        if(~ret)return ret;
-        ret = 0;
-        ret=max(ret,solve(i,len+1,n,s));
-
-        if(search(s.substr(i,len)))
-            ret=max(ret,solve(i+len,1,n,s));
-        return ret;
-    }
+    
     bool wordBreak(string s, vector<string>& wordDict) {
-        trie = new TrieNode();
-
-        for(auto s : wordDict){
-            insert(s);
-        }
-        memset(dp,-1,sizeof dp);
-        return solve(0,1,s.size(),s);
+         unordered_set<string> st(wordDict.begin(),wordDict.end());
+         vector<int>dp(303);
+         dp[0] = 1;
+         int n = s.size();
+         for(int i = 0 ; i < n;i++){
+            for(int len = 1; len <= n; len++){
+                if(dp[i] && i+len <= n && st.count(s.substr(i,len)))
+                    dp[i+len] = true;
+            }
+         }
+         return dp[n];
 
     }
 };
