8687c8fffa3c1e69ef5f00c289ef4f731d8cd663
Hossam Oraby <38508098+MaTb3a@users.noreply.github.com>
2025-08-25 21:41:22 +0300

    Time: 9 ms (69.97%) | Memory: 19.8 MB (42.00%) - LeetSync

--- 498-diagonal-traverse/diagonal-traverse.py
+++ 498-diagonal-traverse/diagonal-traverse.py
@@ -1,12 +1,9 @@
 class Solution:
     def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
         ans = []
-        i,j,idx,idx2=0,0,0,0
+        i,j=0,0,
         n,m = len(mat),len(mat[0])
-        print(n,m)
-        up = 1 # 1 up(x-1,y+1) 0 down (x+1,y-1)
-
-        # 0,0 - 0 1 - 1 0 - 2 0 - 1 1 - 0 2
+        up = 1 
         while len(ans) < n*m:
             while i >= 0 and j >= 0 and i < n and j < m:
                 ans.append(mat[i][j])
@@ -17,7 +14,6 @@ class Solution:
                     i+=1
                     j-=1
           
-            #print(i,j,ans)
             if up:
                 if j < m:
                     i =0
@@ -30,22 +26,7 @@ class Solution:
                 else:
                     i = n -1
                     j+=2
-            # if 2*len(ans) < n*m:
-            #     if i < 0:
-            #         i = 0
-            #         if m == 1:
-            #             i = 1
-            #             j = 0
-            #     if j < 0:
-            #         j = 0
-            # else:
-            #     if j >= m:
-            #         i+=2
-            #         j = m -1
-            #     else:
-            #         i = n -1
-            #         j+=2
-                    
+           
             up = 1 - up
         return ans
 
