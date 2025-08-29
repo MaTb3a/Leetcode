2d4158ba7cd00bad1e65d77a0aff481254606df7
Hossam Oraby <38508098+MaTb3a@users.noreply.github.com>
2025-08-30 00:06:10 +0300

    Time: 0 ms (100.00%) | Memory: 8.6 MB (0.00%) - LeetSync

--- 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.cpp
+++ 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.cpp
@@ -2,9 +2,7 @@ class Solution {
 public:
     long long flowerGame(int n, int m) {
       
-       long long oddN = n&1 ? n/2+1 : n/2, EvenN = n/2;
-       long long oddM = m&1 ? m/2+1 : m/2, EvenM = m/2;
       
-       return oddN*EvenM+EvenN*oddM;
+       return 1LL*(n/2)*(m&1 ? m/2+1 : m/2)+1LL*(n&1 ? n/2+1:n/2)*(m/2);
     }
 };
