3b8bb6a525b140eb3fea3db60ecc0e79f03bd1a0
Hossam Oraby <38508098+MaTb3a@users.noreply.github.com>
2025-09-13 00:35:03 +0300

    Time: 11 ms (34.01%) | Memory: 18.9 MB (95.77%) - LeetSync

---
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool doesAliceWin(string s) {
+         int vowels = 0;
+         for(char ch : s){
+            if(ch == 'a' || ch =='e' || ch =='i' || ch =='o' || ch =='u')vowels++;
+         }
+         return vowels;
+
+        
+    }
+};
