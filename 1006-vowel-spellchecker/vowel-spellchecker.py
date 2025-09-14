5b108f331e51d46580c72cb1f670feab35b0df03
Hossam Oraby <38508098+MaTb3a@users.noreply.github.com>
2025-09-14 18:18:28 +0300

    Time: 53 ms (18.70%) | Memory: 22 MB (5.10%) - LeetSync

--- 1006-vowel-spellchecker/vowel-spellchecker.py
+++ 1006-vowel-spellchecker/vowel-spellchecker.py
@@ -1,42 +1,31 @@
 class Solution:
     def spellchecker(self, wordlist: List[str], queries: List[str]) -> List[str]:
        
-        def is_vowel(c):
-            return c == 'a' or c == 'i' or c == 'e' or c == 'u' or c == 'o'
-        
-        def hashed_string(s):
-            temp_s = s.lower()
-            hashed_s = ""
-            for c in temp_s:
-                if is_vowel(c):
-                    hashed_s+='*'
-                else:
-                    hashed_s+=c
-            return hashed_s
+      
+        def hash_vowel(s):
+            return "".join( '*' if c in "aeiou" else c for c in s.lower())
         
         ans = []              
-        wordlist_exist = set()    
-        wordlist_capitalization = defaultdict(list)
-        wordlist_vowel_error = defaultdict(list)
+        perfect = set(wordlist)    
+        cap = defaultdict(list)
+        vow = defaultdict(list)
 
         for s in wordlist:
-            wordlist_exist.add(s)
-            wordlist_capitalization[s.lower()].append(s) 
-            wordlist_vowel_error[hashed_string(s)].append(s)
+            cap[s.lower()].append(s) 
+            vow[hash_vowel(s)].append(s)
 
-
-      
-        for s in queries:
-            if s in wordlist_exist:
-                ans.append(s)
-            elif s.lower() in wordlist_capitalization:
-                val = wordlist_capitalization[s.lower()][0]
-                ans.append(val)
-            elif hashed_string(s) in wordlist_vowel_error:
-                val = wordlist_vowel_error[hashed_string(s)][0]
-                ans.append(val)
-            else:
-                ans.append("")
+        def solve(q):
+            if q in perfect:
+                return q
+            lq = q.lower()
+            if lq in cap:
+                return cap[lq][0]
+            hash_q = hash_vowel(q)
+            if hash_q in vow:
+                return vow[hash_q][0]
+            return ""
         
-        return ans
+        return list(map(solve,queries))
+        
+        
             
