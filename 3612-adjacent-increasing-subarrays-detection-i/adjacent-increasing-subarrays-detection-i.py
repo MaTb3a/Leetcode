eaa007504734e8a0a6bd0ea25027ab76160bc002
Hossam Oraby <38508098+MaTb3a@users.noreply.github.com>
2025-10-14 21:34:35 +0300

    Time: 90 ms (31.55%) | Memory: 17.7 MB (86.63%) - LeetSync

--- 3612-adjacent-increasing-subarrays-detection-i/adjacent-increasing-subarrays-detection-i.py
+++ 3612-adjacent-increasing-subarrays-detection-i/adjacent-increasing-subarrays-detection-i.py
@@ -1,19 +1,13 @@
 class Solution:
     def hasIncreasingSubarrays(self, nums: List[int], k: int) -> bool:
-        if k == 1:
+        def solve(i):
+            for j in range(k-1):
+                if nums[i+j+1] <= nums[i+j]:
+                    return False
             return True
-        i,j=1,k+1
-        while(j < len(nums)):
-            cnt = 0
-            for x in range(k):
-                if j+x >= len(nums):
-                    break
-                if nums[i+x] > nums[i+x-1] and nums[j+x] > nums[j+x-1]:
-                    cnt+=1
-                else:
-                    break
-            print(i,j,cnt)
-            if cnt == k-1:
+        i,j=0,k
+        while(j+k-1 < len(nums)):
+            if solve(i) and solve(j):
                 return True
             i+=1
             j+=1
