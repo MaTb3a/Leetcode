7fdf11d611e084a5e2290d6648a1172d5f769421
Hossam Oraby <38508098+MaTb3a@users.noreply.github.com>
2025-10-16 21:31:34 +0300

    Time: 195 ms (21.93%) | Memory: 38.5 MB (58.49%) - LeetSync

--- 2661-smallest-missing-non-negative-integer-after-operations/smallest-missing-non-negative-integer-after-operations.py
+++ 2661-smallest-missing-non-negative-integer-after-operations/smallest-missing-non-negative-integer-after-operations.py
@@ -1,23 +1,8 @@
 class Solution:
     def findSmallestInteger(self, nums: List[int], value: int) -> int:
         mex = 0
-        mp = {}
-        for i in range(len(nums)):
-            nums[i] = ((nums[i] %value)+value)%value
-            while nums[i] in mp:
-                l,r,cur = 0, 1000000, 0
-                while l <= r:
-                    md = (l+r)//2
-                    if nums[i]+(md*value) not in mp:
-                        r = md -1
-                        cur = md
-                    else:
-                        l = md+1
-                nums[i]+=(cur*value)
-            mp[nums[i]] = True
-        
-       
-        for i in range(len(nums)+1):
-            if i not in mp:
-                return i 
-        return -1
+        mp = Counter(x%value for x in nums)
+        while mp[mex%value] > 0:
+            mp[mex%value]-=1
+            mex+=1
+        return mex
