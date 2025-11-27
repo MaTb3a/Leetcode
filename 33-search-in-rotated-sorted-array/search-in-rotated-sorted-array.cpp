8d4e8f15409ea8c334342c6921abc16bd1c19814
Hossam Oraby <38508098+MaTb3a@users.noreply.github.com>
2025-11-27 15:43:50 +0100

    Time: 0 ms (100.00%) | Memory: 15.2 MB (32.17%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,22 +1,32 @@
 class Solution {
 public:
-    int BS(int st,int ed,int target,vector<int>& nums){
-        if(st > ed)return -1;
-        int md = (st+ed)/2;
-        if(nums[md] == target)return md;
-        if(nums[md] < target)
-            return BS(md+1,ed,target,nums);
-        else
-            return BS(st,md-1,target,nums);
-    }
+    
+    
     int search(vector<int>& nums, int target) {
-        int k = 0;
-        for(int i = 0 ; i < nums.size()-1;i++)
-            if(nums[i] > nums[i+1]){
-                k = i;break;
+        int n= nums.size(),st = 0,ed = n-1,idx = -1;
+        while(st<=ed){
+            int md = (st+ed)/2;
+            if(nums[md] == target)return md;
+
+            if(nums[st] <= nums[md]){ // sorted range(st , md)
+                if(nums[st] <= target && target < nums[md]){
+                    ed = md-1;
+                }
+                else {
+                    st = md+1;
+                }
             }
-        //cout<<k<<"\n";
-        return max(BS(0,k,target,nums),BS(k+1,nums.size()-1,target,nums));
+            else{ // sorted range(md, ed)
+                if(nums[md] <= nums[ed]){
+                    if(nums[md] < target && target <= nums[ed]){
+                        st = md+1;
+                    }
+                    else 
+                        ed = md-1;
+                }
+            }
+        }
+        return -1;
        
     }
 };
