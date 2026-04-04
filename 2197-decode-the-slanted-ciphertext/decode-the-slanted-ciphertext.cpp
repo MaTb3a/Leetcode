783feda82fcfdcca628f76989be390cc5bf01122
Hossam Oraby <38508098+MaTb3a@users.noreply.github.com>
2026-04-04 12:41:17 +0200

    Time: 72 ms (5.93%) | Memory: 60.7 MB (5.08%) - LeetSync

---
+++ 2197-decode-the-slanted-ciphertext/decode-the-slanted-ciphertext.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string decodeCiphertext(string encodedText, int rows) {
+        int m = (encodedText.size())/rows,k=0;
+        vector<string>v(rows);
+        for(int i = 0 ; i < rows ;i++)
+            for(int j = 0 ; j < m;j++)
+                if(k < encodedText.size())
+                    v[i]+=encodedText[k++];
+        
+        string decodedText = "";
+        for(int j = 0 ; j < m;j++){
+            int i = 0,k = j;
+            while(i < rows && k < m)decodedText+=v[i++][k++];
+        }
+        while(decodedText.size() && decodedText.back() == ' ')decodedText.pop_back();
+        return decodedText;
+    }
+};
