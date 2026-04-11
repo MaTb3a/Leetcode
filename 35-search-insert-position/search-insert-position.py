0ec49f0f28929a083710d53b01448a24c9a7cbaf
Hossam Oraby <38508098+MaTb3a@users.noreply.github.com>
2026-04-11 16:50:21 +0200

    Time: 0 ms (100.00%) | Memory: 20 MB (57.71%) - LeetSync

--- 35-search-insert-position/search-insert-position.py
+++ 35-search-insert-position/search-insert-position.py
@@ -1,12 +1,3 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        l,r = 0 ,len(nums)-1
-        while l <= r:
-            md = (l+r)//2
-            if nums[md] == target:
-                return md
-            if nums[md] < target:
-                l = md+1
-            else:
-                r = md -1
-        return l
+        return bisect.bisect_left(nums,target)
