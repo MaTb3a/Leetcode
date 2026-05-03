d4f93f6de650630a3550a52d3c0780bfb2b7ffa4
Hossam Oraby <38508098+MaTb3a@users.noreply.github.com>
2026-05-03 15:29:21 +0200

    Time: 0 ms (100.00%) | Memory: 21 MB (71.07%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,16 +1,7 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        # hash table
+        x = 0
+        for y in nums: x^=y
+        return x
 
-        mp = {}
-
-        for x in nums:
-            if x in mp:
-                mp[x]+=1
-            else:
-                mp[x] = 1
-        
-        for key,val in mp.items():
-            if val == 1:
-                return key
-        return -1
+       
