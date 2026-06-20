55fcf58a3ee4873bb67862cb3e05861cdbc2bf63
Hossam Oraby <38508098+MaTb3a@users.noreply.github.com>
2026-06-20 11:46:05 +0200

    Time: 0 ms (100.00%) | Memory: 19.3 MB (16.83%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,6 +1,7 @@
 class Solution:
     @cache
     def climbStairs(self, n: int) -> int:
-        if n <= 3:
+        if n < 3 :
             return n
         return self.climbStairs(n-2) + self.climbStairs(n-1)
+        
