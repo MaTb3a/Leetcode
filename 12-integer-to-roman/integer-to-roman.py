8c46d4d6a91ecd12dab3ad982ab91dbd128fd4b8
Hossam Oraby <38508098+MaTb3a@users.noreply.github.com>
2026-06-21 11:32:54 +0200

    Time: 11 ms (15.60%) | Memory: 19.3 MB (26.04%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.py
+++ 12-integer-to-roman/integer-to-roman.py
@@ -1,28 +1,23 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        
         dic = {
-            1000:            'M',
-            900:            'CM',
-            500:             'D',
-            400:            'CD',
-            100:             'C',
-            90:             'XC',
-            50:              'L',
-            40:             'XL',
-            10:              'X',
-            9:              'IX',
-            5:               'V',
-            4:              'IV',
-            1:               'I'
+            1000 : 'M',
+            900 : 'CM',
+            500 : 'D',
+            400 : 'CD',
+            100 : 'C',
+            90 : 'XC',
+            50 : 'L',
+            40 : 'XL',
+            10 : 'X',
+            9 : 'IX',
+            5 : 'V',
+            4 : 'IV',
+            1 : 'I',
         }
-        
-        ans = ""
+        ans = ''
         for k,v in dic.items():
             x = num // k
-            ans+= v*x
+            ans += x * v 
             num -= x*k
-
         return ans
-
-
