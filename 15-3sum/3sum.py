df0e533f93979154543a8e1f6968b26d3bf725a9
Hossam Oraby <38508098+MaTb3a@users.noreply.github.com>
2026-07-12 17:41:58 +0200

    Time: 2226 ms (5.01%) | Memory: 23.7 MB (7.04%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -7,11 +7,11 @@ class Solution:
             j = i+1
             k = n-1
             while j < k:
-                if nums[i] + nums[j] + nums[k] == 0:
-                    st.add((nums[i],nums[j],nums[k]))
-                    j+=1
-                elif nums[i] + nums[j] + nums[k] < 0:
+                if nums[i] + nums[j] + nums[k] <= 0:
+                    if nums[i] + nums[j] + nums[k] == 0:
+                        st.add((nums[i],nums[j],nums[k]))
                     j+=1
                 else:
                     k-=1
-        return [list(t) for t in st]
+        return [list(x) for x in st]
+
