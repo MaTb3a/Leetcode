c24d02596e260db2046a8ed72d09d6b5a3eb8d99
Hossam Oraby <38508098+MaTb3a@users.noreply.github.com>
2026-08-08 10:12:10 +0200

    Time: 3 ms (74.27%) | Memory: 19.4 MB (34.13%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,36 +1,18 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        def rowCheck(r):
-            vis = set()
-            for i in range(9):
-                if board[r][i] != '.' and board[r][i] in vis:
-                    return False
-                vis.add(board[r][i])
-            return True
-        
-        def colCheck(c):
-            vis = set()
-            for i in range(9):
-                if board[i][c] != '.' and board[i][c] in vis:
-                    return False
-                vis.add(board[i][c])
-            return True
-
-        def subCheck(r,c):
-            vis = set()
-            for i in range(3):
-                for j in range(3):
-                    if board[r+i][c+j] != '.' and board[r+i][c+j] in vis:
-                        return False
-                    vis.add(board[r+i][c+j])
-            return True
+        row = [set() for _ in range(9)]
+        col = [set() for _ in range(9)]
+        box = [set() for _ in range(9)]
 
         for i in range(9):
-            if not rowCheck(i) or not colCheck(i):
-                return False
-        for i in range(3):
-            for j in range(3):
-                if not subCheck(i*3,j*3):
+            for j in range(9):
+                if board[i][j] == '.':
+                    continue
+                idx = (i//3)*3 + (j//3)
+                if board[i][j] in row[i] or board[i][j] in col[j] or board[i][j] in box[idx]:
                     return False
+                
+                row[i].add(board[i][j])
+                col[j].add(board[i][j])
+                box[idx].add(board[i][j])
         return True
-
