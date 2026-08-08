248f4461bf4f7dda674c23db39b11b722081c4ba
Hossam Oraby <38508098+MaTb3a@users.noreply.github.com>
2026-08-08 09:40:58 +0200

    Time: 251 ms (9.61%) | Memory: 19.9 MB (13.39%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -3,12 +3,11 @@ class Solution:
         vis = set()
         r = mx = 0
         for l in range(len(s)):
-            while r < len(s) and s[r] not in vis:
+            while r < len(s) and s[r] not in vis: 
                 vis.add(s[r])
                 r+=1
-                mx = max(mx,r-l)
+                mx=max(mx,r-l) 
             vis.remove(s[l])
-            
         return mx
-
+            
 
