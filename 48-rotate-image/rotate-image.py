e7fdee4a5b0366b66f365d37090213aa281781a0
Hossam Oraby <38508098+MaTb3a@users.noreply.github.com>
2026-08-29 08:41:17 +0200

    Time: 0 ms (100.00%) | Memory: 19.3 MB (28.74%) - LeetSync

--- 48-rotate-image/rotate-image.py
+++ 48-rotate-image/rotate-image.py
@@ -4,9 +4,11 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         """
         n = len(matrix)
-        for i in range( n //2):
-            for j in range(i,n-1-i):
-                matrix[i][j],matrix[j][n-i-1] = matrix[j][n-i-1], matrix[i][j]
-                matrix[i][j],matrix[n-1-j][i] = matrix[n-1-j][i],matrix[i][j]
-                matrix[n-1-j][i],matrix[n-i-1][n-j-1]=matrix[n-i-1][n-j-1],matrix[n-1-j][i]
-        return matrix
+        for i in range(n):
+            for j in range(i+1,n):
+                matrix[i][j],matrix[j][i] = matrix[j][i],matrix[i][j]
+        
+        print(matrix)
+        for i in range(n):
+            matrix[i] = matrix[i][::-1]
+
