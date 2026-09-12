92780c30ec01f08f0ad82007372092179ed5938e
Hossam Oraby <38508098+MaTb3a@users.noreply.github.com>
2026-09-12 10:13:30 +0200

    Time: 221 ms (12.95%) | Memory: 20.7 MB (83.90%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -1,17 +1,23 @@
-class Solution(object):
-    def setZeroes(self, matrix):
-        zeroinFirstCol = False
-        for row in range(len(matrix)):
-            if matrix[row][0] == 0:
-                zeroinFirstCol = True
-            for col in range(1, len(matrix[0])):
-                if matrix[row][col] == 0:
-                    matrix[row][0] = 0
-                    matrix[0][col] = 0
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        
+        row,col = [],[]
+        m,n = len(matrix),len(matrix[0])
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j] == 0:
+                    row.append(i)
+                    col.append(j)
+        
+        for r in row:
+            for i in range(n):
+                matrix[r][i] = 0
 
-        for row in range(len(matrix) - 1, -1, -1):
-            for col in range(len(matrix[0]) - 1, 0, -1):
-                if matrix[row][0] == 0 or matrix[0][col] == 0:
-                    matrix[row][col] = 0
-            if zeroinFirstCol:
-                matrix[row][0] = 0
+        for c in col:
+            for j in range(m):
+                matrix[j][c] = 0
+        
+        
