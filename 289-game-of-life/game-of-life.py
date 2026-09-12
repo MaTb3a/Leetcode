284fe3752fb506795f112f1b6c186e05ef2a2aff
Hossam Oraby <38508098+MaTb3a@users.noreply.github.com>
2026-09-12 11:35:51 +0200

    Time: 0 ms (100.00%) | Memory: 19.3 MB (49.54%) - LeetSync

--- 289-game-of-life/game-of-life.py
+++ 289-game-of-life/game-of-life.py
@@ -4,34 +4,38 @@ class Solution:
         Do not return anything, modify board in-place instead.
         """
         n,m = len(board),len(board[0])
-        newBoard = [[0 for j in range(m)] for i in range(n)]
 
         dx = [0,0,1,-1,1,1,-1,-1]
         dy = [1,-1,0,0,1,-1,1,-1]
+        # 0 2 | 1 3
 
+        # 0 > 0
+        # 0 > 1
+        # 1 > 0
+        # 1 > 1
         def valid(x,y):
             return x >= 0 and y >=0 and x < n and y < m
+        
         for i in range(n):
             for j in range(m):
-            
                 cnt = 0
                 for k in range(8):
                     ddx = dx[k]+i
                     ddy = dy[k]+j
-                    if valid(ddx,ddy) and board[ddx][ddy] == 1:
-                        cnt+=1
-                if board[i][j] == 0:
-                    newBoard[i][j] = cnt == 3
+                    if valid(ddx,ddy):
+                        cnt+=board[ddx][ddy]&1
+
+                if board[i][j] == 0 :
+                    if cnt == 3:
+                        board[i][j] |=2
                 
                 else:
-                    newBoard[i][j] = cnt == 2 or cnt == 3
+                    if cnt == 2 or cnt == 3:
+                        board[i][j]|=2
         
         for i in range(n):
             for j in range(m):
-                if newBoard[i][j]:
-                    board[i][j] = 1
-                else:
-                    board[i][j] = 0
+                board[i][j] >>=1
         
                     
 
